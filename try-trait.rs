ef2a333ec3057caa88490345e3b3b4f4a0f1dad5
Bart Massey <bart.massey@gmail.com>
2024-03-19 22:36:40 -0700

    cleaned up error handling

--- try-trait.rs
+++ try-trait.rs
@@ -1,38 +1,28 @@
-/*
-macro_rules! try_option {
-    ($x:expr) => {
-        match $x {
-            Some(y) => y,
-            None => return None,
-        }
-    }
-}
-*/
+use std::convert::TryFrom;
+use thiserror::Error;
 
 fn collatz_step(x: u8) -> Option<String> {
-    Some(x.checked_mul(2)?.checked_add(1)?.to_string())
+    Some(x.checked_mul(3)?.checked_add(1)?.to_string())
 }
-
-use thiserror::Error;
 
 #[derive(Debug, Error)]
 enum CollatzifyError {
     #[error("out-of-domain value")]
-    DomainError,
+    DomainError(#[source] <u8 as TryFrom<u16>>::Error),
     #[error("collatz overflow")]
     CollatzOverflow,
-    #[error("parse error")]
-    ParseError,
 }
 use CollatzifyError::*;
 
 fn collatzify(x: u16) -> Result<u8, CollatzifyError> {
-    let x = x.try_into().map_err(|_| DomainError)?;
+    let x = x.try_into().map_err(DomainError)?;
     let s = collatz_step(x).ok_or(CollatzOverflow)?;
-    s.parse().map_err(|_| ParseError)
+    Ok(s.parse().unwrap())
 }
 
 fn main() {
-    println!("{}", collatz_step(127).unwrap());
-    println!("{}", collatzify(127).unwrap());
+    println!("{}", collatz_step(84).unwrap());
+    println!("{}", collatzify(84).unwrap());
+    println!("{:?}", collatzify(85));
+    println!("{:?}", collatzify(65_534));
 }
